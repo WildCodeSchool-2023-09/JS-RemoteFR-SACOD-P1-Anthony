22f374d110b499e6f19a93220cc177725bc7a2fd
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-04 16:13:20 +0200

    localstorage done, style done

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,148 @@
 /**
- * Tous mes selectors
+ * Tous mes selectors html
  */
 const categories = document.querySelector("#categories");
 const form = document.querySelector("form");
 const todo = document.querySelector("#todo");
 const category = document.querySelector("#category");
 const displayAllTodos = document.querySelector(".allTodos");
+// allTodos est un tableau qui contiendra tous les todos
+const allTodos = [];
 
-let allTodos = [];
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
+/**
+ * La fonction `onload` est appelée lorsque la page est chargée
+ * Elle me permet d'initialiser mon application
+ */
+onload = () => {
+	// Je récupère mes todos dans le localStorage
+	const todos = JSON.parse(localStorage.getItem("todos"));
+	if (!todos) return;
+	todos.forEach((todo) => {
+		displayOnLoad(todo);
+	});
+};
+
+/** Cette fonction, me permet d'afficher et d'interagire avec mes anciens todos */
+function displayOnLoad(todo) {
+	// Je créer un li
 	const newTodo = document.createElement("li");
+	newTodo.classList.add("wip");
+	const span = document.createElement("span");
+	span.textContent = todo.text;
+	newTodo.appendChild(span);
+	newTodo.dataset.category = todo.category;
+
+	// Si le todo est checké, j'ajoute la classe checked
+	if (todo.checked) {
+		newTodo.classList.add("checked");
+	}
+
+	const containerButtons = document.createElement("span");
+
+	/** Button check */
+	const isCheck = document.createElement("button");
+	isCheck.textContent = todo.checked ? "❌" : "✅";
+	isCheck.classList.add("check");
+	containerButtons.appendChild(isCheck);
+
+	/** J'écoute ce button, pour savoir si je met un ✅ ou ❌ */
+	isCheck.addEventListener("click", () => {
+		newTodo.classList.toggle("checked");
+		newTodo.classList.contains("checked")
+			? (isCheck.textContent = "❌")
+			: (isCheck.textContent = "✅");
+		// Puis je sauvegarde mes todos
+		saveTodos();
+	});
+
+	/** Button supprimer */
+	const del = document.createElement("button");
+	del.textContent = "🗑️";
+	del.classList.add("delete");
+	containerButtons.appendChild(del);
+
+	// Si le button est cliqué, je supprime le li
+	del.addEventListener("click", () => {
+		newTodo.remove();
+		allTodos.splice(allTodos.indexOf(newTodo), 1);
+
+		// Puis je sauvegarde mes todos
+		saveTodos();
+	});
+
+	newTodo.appendChild(containerButtons);
+
+	displayAllTodos.appendChild(newTodo);
+	allTodos.push(newTodo);
+}
+
+function createTodo(todo, category) {
+	const newTodo = document.createElement("li");
+	newTodo.classList.add("wip");
+
+	// si le champ est vide, je ne fais rien
 	if (todo.value === "") return;
-	newTodo.textContent = todo.value;
+
+	const containerButtons = document.createElement("span");
+
+	// Je créer une span, pour accèder plus facilement au contenu du texte
+	const span = document.createElement("span");
+	span.textContent = todo.value;
+	newTodo.appendChild(span);
 	newTodo.dataset.category = category.value;
+
+	/** Button check */
+	const isCheck = document.createElement("button");
+	isCheck.textContent = todo.checked ? "❌" : "✅";
+	isCheck.classList.add("check");
+	containerButtons.appendChild(isCheck);
+
+	/** J'écoute ce button, pour savoir si je met un ✅ ou ❌ */
+	isCheck.addEventListener("click", () => {
+		newTodo.classList.toggle("checked");
+		newTodo.classList.contains("checked")
+			? (isCheck.textContent = "❌")
+			: (isCheck.textContent = "✅");
+		// Puis je sauvegarde mes todos
+		saveTodos();
+	});
+
+	const del = document.createElement("button");
+	del.textContent = "🗑️";
+	del.classList.add("delete");
+	containerButtons.appendChild(del);
+
+	// if del is clicked
+	del.addEventListener("click", () => {
+		// remove the li from the ul
+		newTodo.remove();
+		allTodos.splice(allTodos.indexOf(newTodo), 1);
+		console.log(allTodos);
+		saveTodos();
+	});
+
+	newTodo.appendChild(containerButtons);
 
 	displayAllTodos.appendChild(newTodo);
 	todo.value = "";
 
-	getAllTodos(document.querySelectorAll(".allTodos li"));
+	allTodos.push(newTodo);
+}
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	createTodo(todo, category);
+	saveTodos();
 });
 
-function getAllTodos(todos) {
-	allTodos = [...todos];
-}
+const saveTodos = () => {
+	const todos = allTodos.map((todo) => ({
+		text: todo.childNodes[0].textContent,
+		checked: todo.classList.contains("checked"),
+		category: todo.dataset.category,
+	}));
+	localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 categories.addEventListener("change", (event) => {
 	if (event.target.value === "all") {
@@ -33,7 +152,6 @@ categories.addEventListener("change", (event) => {
 		const filteredTodos = allTodos.filter(
 			(todo) => todo.dataset.category === event.target.value
 		);
-		console.log(filteredTodos);
 		displayAllTodos.innerHTML = "";
 		filteredTodos.forEach((todo) => displayAllTodos.appendChild(todo));
 	}
