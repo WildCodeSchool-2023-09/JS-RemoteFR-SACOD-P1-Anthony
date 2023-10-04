5946775468e245d2d299489a0d14e535d338aded
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-04 11:09:37 +0200

    add filtered button

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,40 @@
+/**
+ * Tous mes selectors
+ */
 const categories = document.querySelector("#categories");
+const form = document.querySelector("form");
+const todo = document.querySelector("#todo");
+const category = document.querySelector("#category");
+const displayAllTodos = document.querySelector(".allTodos");
+
+let allTodos = [];
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const newTodo = document.createElement("li");
+	if (todo.value === "") return;
+	newTodo.textContent = todo.value;
+	newTodo.dataset.category = category.value;
+
+	displayAllTodos.appendChild(newTodo);
+	todo.value = "";
+
+	getAllTodos(document.querySelectorAll(".allTodos li"));
+});
+
+function getAllTodos(todos) {
+	allTodos = [...todos];
+}
 
 categories.addEventListener("change", (event) => {
 	if (event.target.value === "all") {
-		console.log(event.target.value);
+		displayAllTodos.innerHTML = "";
+		allTodos.forEach((todo) => displayAllTodos.appendChild(todo));
 	} else {
-		console.log(event.target.value);
+		const filteredTodos = allTodos.filter(
+			(todo) => todo.dataset.category === event.target.value
+		);
+		console.log(filteredTodos);
+		displayAllTodos.innerHTML = "";
+		filteredTodos.forEach((todo) => displayAllTodos.appendChild(todo));
 	}
 });
